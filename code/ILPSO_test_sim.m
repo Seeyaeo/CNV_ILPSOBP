d47bbf3325de2f71fc61370260ad0dfd78235dea
Seeyaeo <zhumengxuan0912@163.com>
2025-02-05 20:50:05 +0800

    ILPSO_test_sim.m

--- code/ILPSO_test_sim.m
+++ code/ILPSO_test_sim.m
@@ -1,12 +1,13 @@
-%%加载神经网络
+%% Clear environment variables
 clc
 clear
 
 %% Import the parameters of the trained neural network
-data_truth1=load('groundtruth_11_1.mat');   %Import groundtruth file
+data_truth1=load('groundtruth_11_1.mat');   
+%% Import groundtruth file
 data_truth=data_truth1.('data_truth');
 
-%% 读取数据集
+%% Obtain normalized information in training samples
 data_trains1=load('data\0.2_4x_11.1\sim1_4_4100_trains.txt');
 data_trains2=load('data\0.3_4x_11.1\sim1_4_4100_trains.txt');
 data_trains3=load('data\0.4_4x_11.1\sim1_4_4100_trains.txt');
@@ -27,15 +28,14 @@ column3=[1,2];
 groundtruth=data_truth(:,column3); 
 gtRev=fliplr(groundtruth(:,2)'); 
 
-%从1到trainlines间随机排序
+%Random sorting
 k=rand(1,trainLines);
 [m,n]=sort(k);
 
-%得到输入输出数据
+%Get input and output data
 ginput=gdata(:,column);
 goutput1 =gdata(:,7);
 
-%输出从一维变成四维: 0——normal，1——gain，2——hemi_loss，3——homo_loss;
 goutput=zeros(trainLines,4);
 for i=1:trainLines
     switch goutput1(i)
@@ -50,15 +50,14 @@ for i=1:trainLines
     end
 end
 
-%找出训练数据和预测数据
+%Find the training data and prediction data
 ginput_train=ginput(n(1:trainLines),:)';
 goutput_train=goutput(n(1:trainLines),:)';
 
-%样本输入输出数据归一化
+%Normalization
 [ginputn,ginputps]=mapminmax(ginput_train);
 [outputn,outputps]=mapminmax(goutput_train);
 
-%%加载网络
 load('-mat','PSOBP');
 
 %% 3.Load test data and count results
@@ -67,14 +66,14 @@ TP_count_sum=0;
 TPFP_count_sum=0;
 bound = 0;
 count_bias_sum=0;
-boundary=[]; %每个bam的边界，用于生成箱线图
+boundary=[]; 
 num_boundary=0;
 flag=1;
 normal_sum=zeros(6,50);
 gain_sum=zeros(6,50);
 hemi_sum=zeros(6,50);
 homo_sum=zeros(6,50);
-% 3.1 循环加载测试数据
+
 % mkdir('data\','0.2_4x_binnumber');
 % mkdir('data\','0.2_4x_allbinnumber');
 mkdir('data\','0.2_6x_binnumber');
@@ -88,6 +87,7 @@ mkdir('data\','0.2_6x_allbinnumber');
 % mkdir('data\','0.4_6x_binnumber');
 % mkdir('data\','0.4_6x_allbinnumber');
 
+%Importing test sample data
 for t=1:num
     data_tests=load(['C:\Users\Zmx\Desktop\IPSOCNV_Q\data\0.2_6x_11.1\sim', num2str(t) ,'_6_6100_trains.txt']);
     [m2,n2] = size(data_tests);
@@ -111,53 +111,46 @@ for t=1:num
      end
     ginput_test=ginput2((1:testLines),:)';
     goutput_test=goutput2((1:testLines),:)';
-    %% BP网络预测
-     % 4.1预测数据归一化
-     inputn_test=mapminmax('apply',ginput_test,ginputps);
 
-     % 4.2网络预测输出
-     an=sim(net,inputn_test);
+    %Obtain normalized test samples
+    inputn_test=mapminmax('apply',ginput_test,ginputps);
+    %inverse normalization
+    an=sim(net,inputn_test);
+    BPoutput=mapminmax('reverse',an,outputps);
+    %Statistical prediction error
+    error=BPoutput-goutput_test;
+    abs_error=abs(error);
+    errorsum=sum(abs(error));
 
-     % 4.3 网络输出反归一化
-     BPoutput=mapminmax('reverse',an,outputps);
+    fid=fopen(['data\0.2_6x_binnumber\sim', num2str(t) ,'_6_6100_bin_number.txt'],'wt'); 
 
-     % 4.4预测误差
-     error=BPoutput-goutput_test;
-     abs_error=abs(error);
-     errorsum=sum(abs(error));
-
-     % 4.5 创建文件保存检测正确的CNV编号
-     fid=fopen(['data\0.2_6x_binnumber\sim', num2str(t) ,'_6_6100_bin_number.txt'],'wt'); 
-
-     TP_count = 0;
-     P_count = 0;
-     TPFP_count = 0;
-     k = 1;
-     kkk = 1;
+    TP_count = 0;
+    P_count = 0;
+    TPFP_count = 0;
+    k = 1;
+    kkk = 1;
             
-     % 4.6 将检测正确的CNV编号写入文件
-     for qq = 1:testLines
-         if ( error(2,qq) < error(1,qq) && error(2,qq) < error(3,qq) && error(2,qq) < error(4,qq) && goutput_test(2,qq) == 1)
-             fprintf(fid,'%d\t',ginput2_bin(qq));
-             fprintf(fid,'gain\t1');
-             fprintf(fid,'\n');
-             continue;
-         end
-         if ( error(3,qq) < error(1,qq) && error(3,qq) < error(2,qq) && error(3,qq) < error(4,qq) && goutput_test(3,qq) == 1)
-             fprintf(fid,'%d\t',ginput2_bin(qq));
-             fprintf(fid,'hemi_loss\t2');
-             fprintf(fid,'\n');
-             continue;
-         end
-         if ( error(4,qq) < error(1,qq) && error(4,qq) < error(2,qq) && error(4,qq) < error(3,qq) && goutput_test(4,qq) == 1)
-             fprintf(fid,'%d\t',ginput2_bin(qq));
-             fprintf(fid,'homo_loss\t3');
-             fprintf(fid,'\n');
-             continue;
-         end
-    end
+    for qq = 1:testLines
+        if ( error(2,qq) < error(1,qq) && error(2,qq) < error(3,qq) && error(2,qq) < error(4,qq) && goutput_test(2,qq) == 1)
+            fprintf(fid,'%d\t',ginput2_bin(qq));
+            fprintf(fid,'gain\t1');
+            fprintf(fid,'\n');
+            continue;
+        end
+        if ( error(3,qq) < error(1,qq) && error(3,qq) < error(2,qq) && error(3,qq) < error(4,qq) && goutput_test(3,qq) == 1)
+            fprintf(fid,'%d\t',ginput2_bin(qq));
+            fprintf(fid,'hemi_loss\t2');
+            fprintf(fid,'\n');
+            continue;
+        end
+        if ( error(4,qq) < error(1,qq) && error(4,qq) < error(2,qq) && error(4,qq) < error(3,qq) && goutput_test(4,qq) == 1)
+            fprintf(fid,'%d\t',ginput2_bin(qq));
+            fprintf(fid,'homo_loss\t3');
+            fprintf(fid,'\n');
+            continue;
+        end
+   end
             
-      % 4.8 创建文件保存所有检测的CNV编号（无论对错都保存下来），少了判断真实值是1的条件
       fidall=fopen(['data\0.2_6x_allbinnumber\sim', num2str(t) ,'_6_6100_allbinnumber.txt'],'wt'); 
       u=1;
       for p=1:testLines
@@ -181,12 +174,12 @@ for t=1:num
           end
       end    
       fclose(fidall);
-            
+      
+      % calculate TP_count,P_count,TPFP_count,boundary
       binnumber=[];
       binnumber_tpfp=[];
             
       for q=1:testLines
-      % 4.7 统计TP_count,P_count,TPFP_count,boundary
           if (( abs_error(2,q) < abs_error(1,q) && abs_error(2,q) < abs_error(3,q) && abs_error(2,q) < abs_error(4,q) && goutput_test(2,q) == 1) || ( abs_error(3,q) < abs_error(1,q) && abs_error(3,q) < abs_error(2,q) && abs_error(3,q) < abs_error(4,q) && goutput_test(3,q) == 1) || (abs_error(4,q) < abs_error(1,q) && abs_error(4,q) < abs_error(2,q) && abs_error(4,q) < abs_error(3,q) && goutput_test(4,q) == 1)) 
               TP_count = TP_count+1;
               binnumber(k) = ginput2_bin(q);
@@ -207,8 +200,7 @@ for t=1:num
       TP_count_sum = TP_count_sum + TP_count;
       boundbias = (P_count - TP_count)./14;
       bound = bound + boundbias;
-    
-      % 将检测到的14条cnv的bin开头结尾的编号保存下来
+   
       binnumberRev = fliplr(binnumber);
       [m3,n3] = size(binnumber);
       jj = 1;
@@ -240,7 +232,6 @@ for t=1:num
           continue; 
       end
       bin = [bin1;bin2]'; 
-      % 计算边界精度
       [m4,n4] = size(bin);
       c_bias1 = [];
       c_bias2 = [];
@@ -259,18 +250,21 @@ for t=1:num
       binnumber_fp = setdiff(binnumber_tpfp,binnumber);
 end
 
-%% 结果分析 
+
 TP_count_avg = TP_count_sum / num;
 TPFP_count_avg = TPFP_count_sum / num;
+%Calculating the sensitivity
 sensitivity = TP_count_avg / P_count;
+%Calculating the precision
 precision = TP_count_avg / TPFP_count_avg;
+%Calculating theF1-score
 F1_score = (2 * sensitivity * precision)/(sensitivity + precision);
 boundary_bias = bound / num;
 count_bias_avg = count_bias_sum / num_boundary;
 TPR = sensitivity;
 FPTN_count = m2 - P_count;
 FP_count = TPFP_count - TP_count;
-FPR=FP_count/FPTN_count; %计算FPR
+FPR=FP_count/FPTN_count; 
         
 disp('sensitivity:');
 disp(sensitivity);
